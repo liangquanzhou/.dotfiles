83bf257f20c610534c2649f47eb28a80cd4cabbd
zhouliangquan <zhouliangquan@didiglobal.com>
2022-11-03 21:54:40 +0800

    update zsh config, add custom program

---
+++ shell/.config/zsh/program/wake/wake.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This script disables sleeping on lid close
+SUDO=""
+(( $EUID > 0 )) && SUDO="sudo -n"
+function finish {
+    $SUDO pmset disablesleep 0
+    printf "\nSleep has been re-enabled"
+}
+$SUDO pmset disablesleep 1 || exit
+trap finish EXIT
+printf "Sleep disabled... quit script to re-enable (Ctrl+C)\n"
+while :
+	do sleep 1000
+done
